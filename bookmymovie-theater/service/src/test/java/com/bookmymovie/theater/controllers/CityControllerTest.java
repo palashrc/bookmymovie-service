0c844dd71e86ff45a2fe5f97ace594fff5a3b7b6
palashrc <connect.palash@gmail.com>
2024-08-22 21:41:52 +0530

    Changes for Jacoco in CityController in Theater service

--- bookmymovie-theater/service/src/test/java/com/bookmymovie/theater/controllers/CityControllerTest.java
+++ bookmymovie-theater/service/src/test/java/com/bookmymovie/theater/controllers/CityControllerTest.java
@@ -3,44 +3,25 @@ package com.bookmymovie.theater.controllers;
 import com.bookmymovie.theater.model.CityRequest;
 import com.bookmymovie.theater.model.CityResponse;
 import com.bookmymovie.theater.service.CityService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
-@TestPropertySource(properties = "classpath:application.yml")
-@WebMvcTest(CityController.class)
 class CityControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Mock
+    CityService cityService;
 
-    @MockBean
-    private CityService mockCityService;
+    @InjectMocks
+    CityController cityController;
 
-    //@Test //Error for this Annotation
+    @Test
     void testAddCity() throws Exception {
-        // Setup:
-        //when(mockCityService.saveCity(CityRequest.builder().build())).thenReturn(CityResponse.builder().build());
-
-        // Run the test and verify the results:
-       /*
-       mockMvc.perform(post("/city/city-new")
-                        .content("content").contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().json("{}", true));
-       */
+        CityResponse cityResponse = cityController.addCity(CityRequest.builder().build());
+        Assertions.assertNull(cityResponse);
     }
 }
