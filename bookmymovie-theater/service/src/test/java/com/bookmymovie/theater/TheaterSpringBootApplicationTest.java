9a1a3fed339fdba79bdc28c11868db956c4453b2
palashrc <connect.palash@gmail.com>
2024-08-22 16:34:05 +0530

    Changes or Jacoco in Theater service

--- bookmymovie-theater/service/src/test/java/com/bookmymovie/theater/TheaterSpringBootApplicationTest.java
+++ bookmymovie-theater/service/src/test/java/com/bookmymovie/theater/TheaterSpringBootApplicationTest.java
@@ -1,36 +1,13 @@
 package com.bookmymovie.theater;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class TheaterSpringBootApplicationTest {
-  @Value("${spring.application.name}")
-  String applicationName;
 
-  @Autowired TestRestTemplate template;
-
-  @ParameterizedTest
-  @CsvSource({
-    "/, This is auto-generated '%s' application",
-    "/hello, Hello Anonymous!",
-    "/hello/Sabre, Hello Sabre!",
-    "/smoke, OK"
-  })
-  public void shouldReturnMessageFromPath(String path, String response) {
-    // when
-    ResponseEntity<String> responseEntity = template.getForEntity(path, String.class);
-
-    // then
-    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(responseEntity.getBody()).isEqualTo(String.format(response, applicationName));
+  @Test
+  void contextLoads() {
+    TheaterSpringBootApplication.main(new String[0]);
   }
 }
